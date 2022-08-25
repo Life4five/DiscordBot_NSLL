dd0d8a84588dd585251635f8a3ffb12796a22473
Life4five <dke-rus@mail.ru>
2022-08-25 13:53:41 +0300

    All money confirmation and math problems to work

--- config.py
+++ config.py
@@ -13,11 +13,23 @@ db.commit()
 
 TOKEN = 'OTUwMDAyMDAzODg1NTU1Nzky.GhVKjz.PycQl0afPHDao6qXPWFWgjej9n74QUKmTV52kE'
 
-PREFIX = '$'
-SYSTEM_CHANNEL_ID = 815614417827397652  # channel where the messages will be sent ## admin-chat
+PREFIX = ['!!', '$']
+SYSTEM_CHANNEL_ID = 815611328323911721  # channel where the messages will be sent ## admin-chat
 CURRENCY_SYMBOL = ':coin:'
 
+# Economy and commands cooldown (s)
+CD_WORK = 60
+CD_TOSS = 5
+#
+
 bot = commands.Bot(command_prefix=PREFIX, intents=discord.Intents.all())
+
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    await bot.process_commands(message)
 
 
 @bot.event
--- main.py
+++ main.py
@@ -6,8 +6,11 @@ from config import db
 from config import CURRENCY_SYMBOL as COIN
 from discord.ext import commands
 import random
+import asyncio
 
 
+# Commands
+# General
 @bot.command(description='Returns your message')
 async def echo(ctx, *msg):
     await ctx.send(' '.join(msg))
@@ -24,6 +27,14 @@ async def cmds(ctx):
     await ctx.send(embed=embed)
 
 
+@commands.has_permissions(administrator=True)
+@bot.command(description='**[ADMIN ONLY]** Deletes last n-messages')
+async def clear(ctx, amount: int):
+    await ctx.channel.purge(limit=amount)
+    await ctx.send(f'Cleared `{amount}` messages!', delete_after=3)
+
+
+# Database and economy
 @commands.has_permissions(administrator=True)
 @bot.command(description='**[ADMIN ONLY]** SQL queries executing')
 async def sql(ctx, *query):
@@ -52,18 +63,21 @@ async def reset_database(ctx):
 @bot.command(description='**[ADMIN ONLY]** Zero balance to all members', aliases=['reset-economy'])
 async def reset_economy(ctx):
     cursor.execute("UPDATE users set Balance = 0 WHERE  Balance != 0")
-    cursor.execute("UPDATE server set Bank = 1000000000")
+    cursor.execute("UPDATE server set Bank = 1000000000 WHERE Bank != 1000000000")
     db.commit()
     await ctx.send("Economy restored!")
 
 
-@bot.command(description='Shows budget of the server', aliases=['server-stats'])
+@bot.command(description='Shows budget of the server', aliases=['server-stats', 'stats'])
 async def server_stats(ctx):
-    embed = discord.Embed(title='Title', description='description', color=0)
-    cursor.execute("SELECT * FROM server")
-    embed.add_field(name='Bank', value='', inline=True)
-    embed.add_field(name='Members', value='', inline=True)
-    embed.add_field(name='Both', value='', inline=True)
+    bank_bal = cursor.execute("SELECT * FROM server").fetchone()[0]
+    members_bal = sum([x[0] for x in cursor.execute("SELECT Balance FROM users").fetchall()])
+    both_bal = bank_bal + members_bal
+    embed = discord.Embed(title='Server stats', description='All budget of the server', color=0)
+    embed.add_field(name='Bank', value=f'{COIN} {"{:,}".format(bank_bal)}', inline=True)
+    embed.add_field(name='Members', value=f'{COIN} {"{:,}".format(members_bal)}', inline=True)
+    embed.add_field(name='Both', value=f'{COIN} {"{:,}".format(both_bal)}', inline=True)
+    await ctx.send(embed=embed)
 
 
 @bot.command(description='Know your balance', aliases=['bal', 'ифд', 'ифдфтсу'])
@@ -74,30 +88,23 @@ async def balance(ctx, user: discord.Member = 0):
     await ctx.send(embed=embed)
 
 
-@bot.command(description='The most rich members', aliases=['top-money', 'top-m'])
-async def top_money(ctx, users: int = 10):
-    embed = discord.Embed(title='Top members by', description='**Money**', color=0xFFD700)
-    cursor.execute(f"SELECT Nickname, Balance FROM users ORDER BY Balance DESC LIMIT {users}")
-    for user, user_bal in cursor.fetchall():
-        embed.add_field(name=f'{user}', value=f'{COIN} {user_bal}', inline=False)
-    await ctx.send(embed=embed)
-
-
-@bot.command(description='The most big members', aliases=['top-level', 'top-l'])
-async def top_level(ctx, users: int = 10):
-    embed = discord.Embed(title='Top members by', description='**Level**', color=0xFFD700)
-    cursor.execute(f"SELECT Nickname, Level FROM users ORDER BY Balance DESC LIMIT {users}")
-    for user, user_level in cursor.fetchall():
-        embed.add_field(name=f'{user}', value=f'🔰 {user_level}', inline=False)
+@bot.command(description='The most rich members')
+async def top(ctx, users: int = 10):
+    embed = discord.Embed(title=f'Top {users} members', description='All time', color=0xFFD700)
+    cursor.execute(f"SELECT Nickname, Balance, Level FROM users ORDER BY Balance DESC, Level DESC LIMIT {users}")
+    users_stats = cursor.fetchall()
+    for user, user_bal, user_level in users_stats:
+        embed.add_field(name=f'{user}', value=f'{COIN} {"{:,}".format(user_bal)} | 🔰 {user_level}', inline=False)
     await ctx.send(embed=embed)
 
 
 @commands.has_permissions(administrator=True)
-@bot.command(description='**[ADMIN ONLY]** Add / Remove money for user', aliases=['transfer-money', 't-m'])
+@bot.command(description='**[ADMIN ONLY]** Add / Remove money for user', aliases=['transfer-money', 't-m', 'tm'])
 async def transfer_money(ctx, user: discord.Member, amount):
     trans_money(user, amount)
     user_bal = get_bal(user)
-    embed = discord.Embed(title='Money was transferred!', description=f'Balance {user.mention}:\n {COIN} {user_bal}',
+    embed = discord.Embed(title='Money was transferred!', description=f'Balance {user.mention}:\n'
+                                                                      f'{COIN} {"{:,}".format(user_bal)}',
                           color=0xFFD700)
     await ctx.send(embed=embed)
 
@@ -109,6 +116,7 @@ def trans_money(user: discord.Member, amount: int):
 
 
 def pay_money(payer: discord.Member, receiver: discord.Member, amount: int):
+    amount = abs(amount)
     cursor.execute(f"UPDATE users set Balance = Balance - {amount} WHERE id = {payer.id}")
     cursor.execute(f"UPDATE users set Balance = Balance + {amount} WHERE id = {receiver.id}")
 
@@ -120,19 +128,90 @@ def get_bal(user: discord.Member):
     return cursor.fetchone()[0]
 
 
-# Money income
-@commands.cooldown(1, 10, commands.BucketType.user)
-@bot.command(description='Legal way to earn money')
-async def work(ctx):
-    income = random.randint(1900, 2100)
-    trans_money(ctx.author, income)
-    embed = discord.Embed(title='Work',
-                          description=f'You earned {COIN} {income} \n Balance: {COIN} {get_bal(ctx.author)}')
+def check_bal(user: discord.Member, amount):
+    user_bal = get_bal(user)
+    if user_bal < amount or user_bal == 0:
+        return True  # True, not enough money
+    else:
+        return False  # False, money is enough
+
+
+async def not_enough_money(ctx):
+    embed = discord.Embed(title='Not enough money!',
+                          description=f'Your balance:\n {COIN} {"{:,}".format(get_bal(ctx.author))}')
     await ctx.send(embed=embed)
 
 
-@bot.command(description='Give your money to friend!') # TODO: Make embed
+def get_random_calc():
+    ops = ['+', '-', '*']
+    num1 = str(random.randint(1, 10))
+    num2 = str(random.randint(5, 20))
+    num3 = str(random.randint(1, 10))
+    op1 = random.choice(ops)
+    op2 = random.choice(ops)
+    res = eval(num1 + op1 + num2 + op2 + num3)
+    return num1, op1, num2, op2, num3, res
+
+
+async def confirm_act(m, ctx):
+    def check(m):
+        return m.author == ctx.author and m.channel == ctx.channel
+
+    await ctx.send('Are you sure? y / n')
+    try:
+        confirm = await bot.wait_for('message', timeout=60, check=check)
+        confirm = confirm.content
+    except asyncio.TimeoutError:
+        await ctx.send('Timeout')
+        confirm = 'n'
+    return confirm == 'y'
+
+
+# Legal money income
+@commands.cooldown(1, cfg.CD_WORK, commands.BucketType.user)
+@bot.command(description='Legal way to earn money')
+async def work(ctx):
+    problem = get_random_calc()
+    solve = '{} {} {} {} {} = ?'.format(problem[0], problem[1], problem[2], problem[3], problem[4])
+    await ctx.send(solve)
+
+    def check(m):
+        return m.author == ctx.author and m.channel == ctx.channel
+
+    try:
+        answer = await bot.wait_for('message', timeout=60, check=check)
+        answer = int(answer.content)
+    except ValueError:
+        await ctx.send('Invalid value!')
+        ctx.command.reset_cooldown(ctx)
+        return
+    except asyncio.TimeoutError:
+        ctx.send('Timeout')
+        ctx.command.reset_cooldown(ctx)
+        return
+
+    if answer != problem[5]:
+        await ctx.send(f'Wrong answer. It was `{problem[5]}`')
+        return
+    income = random.randint(1900, 2100)
+    trans_money(ctx.author, income)
+    author_bal = get_bal(ctx.author)
+    embed = discord.Embed(title=':hammer_pick: Work',
+                          description=f'You earned {COIN} {"{:,}".format(income)} \n'
+                                      f'Balance: {COIN} {"{:,}".format(author_bal)}')
+    await ctx.send(embed=embed)
+
+
+@bot.command(description='Give your money to friend!')
 async def pay(ctx, user: discord.Member, amount):
+    if amount == 'all':
+        if not await confirm_act(ctx.message, ctx):
+            return
+        amount = get_bal(ctx.author)
+    amount = int(amount)
+    if check_bal(ctx.author, amount):
+        await not_enough_money(ctx)
+        return
     pay_money(ctx.author, user, amount)
     embed = discord.Embed(title=f"Paid to {user} from {ctx.author}",
                           description=f'Balances:\n'
@@ -143,16 +222,22 @@ async def pay(ctx, user: discord.Member, amount):
     await ctx.send(embed=embed)
 
 
+# Crime money income
+
+# TODO: Rob Command
+
 # Games
-@commands.cooldown(1, 5, commands.BucketType.user)
+@commands.cooldown(1, cfg.CD_TOSS, commands.BucketType.user)
 @bot.command(description='Toss the coin, 50% chance')
 async def toss(ctx, bet):
     if bet == 'all':
+        if not await confirm_act(ctx.message, ctx):
+            return
         bet = get_bal(ctx.author)
     bet = abs(int(bet))
     user_bal = get_bal(ctx.author)
     if user_bal < bet or user_bal == 0:
-        await ctx.send('Not enough money!')
+        await not_enough_money(ctx)
         return
     success = random.randint(0, 1)
     if success == 1:
@@ -169,6 +254,29 @@ async def toss(ctx, bet):
                               description=f'You lost.. \n\n **Your balance:**\n {COIN} {"{:,}".format(user_bal)}',
                               color=0xaa0000)
         await ctx.send(embed=embed)
+
+
+# shop
+@bot.command(description='Shows all goods in the shop')
+async def shop(ctx):
+    embed = discord.Embed(title='Shop', description='All items', color=0)
+    cursor.execute("SELECT * FROM shop")
+    shop_items = cursor.fetchall()
+    for item in shop_items:
+        id = item[0]
+        name = item[1]
+        price = item[2]
+        description = item[3]
+        stock = item[4]
+        embed.add_field(name=f'[{id}] {COIN} {price} — {name}. В наличии: `{stock}`',
+                        value=f'{description}',
+                        inline=False)
+    await ctx.send(embed=embed)
+
+
+@bot.command(desciption='')  # TODO: Need to write
+async def buy_item(ctx):
+    pass
 
 
 bot.run(cfg.TOKEN)
